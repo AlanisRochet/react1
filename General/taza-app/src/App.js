17fd9d951d3951065ad935458ff2836764617667
Alanis Rochet <alanis.brend@gmail.com>
2022-06-15 21:41:06 -0300

    proyecto react

---
+++ General/taza-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Taza Taza</h1>
+        <p>
+          ¡Llevate una A TU CASA!
+        </p>
+        <h3>Proximamente podras comprar tu taza por este medio</h3>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
