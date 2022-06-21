420d70f2c24ce999e51aa40c8f9c5cd2a103930d
Alanis Rochet <alanis.brend@gmail.com>
2022-06-20 21:50:36 -0300

    agregando

---
+++ General/taza-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <header>
+        <h1>Taza Taza</h1>
+        <h2>¡Llevate una a tu casa!</h2>
+    </header>
+  );
+}
+
+export default Navbar;
